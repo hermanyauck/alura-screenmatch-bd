dd518e0d94cab1e9852a45349da94ffb9d880244
herman <herman@hermanyauck>
2024-06-21 22:35:03 -0300

    1st

--- src/main/java/com/hermanyauck/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/hermanyauck/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,21 @@
 package com.hermanyauck.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.hermanyauck.screenmatch.modelos.ConsumirAPI;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception{
+		var consumoAPI = new ConsumirAPI();
+		var json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+throne&apikey=fab839fa");
+		System.out.println(json);
+	}
 }
